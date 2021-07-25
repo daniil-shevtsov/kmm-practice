9fb4f536de6e353bfcc0ffca204c1d8f780a4ec9
Daniil Shevtsov <me@daniilshevtsov.com>
2021-07-25 16:14:08 +0300

    Make desktop work

--- common/src/commonMain/kotlin/com/example/kmmpractice/common/di/Expect.kt
+++ common/src/commonMain/kotlin/com/example/kmmpractice/common/di/Expect.kt
@@ -2,4 +2,5 @@ package com.example.kmmpractice.common.di
 
 import org.koin.core.module.Module
 
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 expect fun platformModule(): Module
---
+++ common/src/jvmMain/kotlin/com/example/kmmpractice/common/di/Actual.kt
@@ -0,0 +1,6 @@
+package com.example.kmmpractice.common.di
+
+import org.koin.dsl.module
+
+actual fun platformModule() = module {
+}
